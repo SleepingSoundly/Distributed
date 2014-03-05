4367c84d381a4a6201030c31fe168739f879884c
SleepingSoundly <hoag3@illinois.edu>
2014-03-05 16:56:13 -0600

    new stuff
    sockets NOT CONNECTING, not sure why

--- message.py
+++ message.py
@@ -1,10 +1,10 @@
-from __future__ import print_function
+ from __future__ import print_function
 import threading
 import time
 import sys
 import socket
 import os
-
+import random
 proc_number = int(sys.argv[1])
 HOST = 'localhost'    # The remote host
 PORT = 8008              # The same port as used by the server
@@ -12,72 +12,86 @@ PORT = 8008              # The same port as used by the server
 
 
 class MyThread(threading.Thread):
-	def run(self):
-		print("{0} started!".format(self.getName()))
-		s = None
+        def run(self):
+                print("{0} started!".format(self.getName()))
+                s = None
                       # "Thread-x started!"
-		for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-			af, socktype, proto, canonname, sa = res
-			try:
-				s = socket.socket(af, socktype, proto)
-			except socket.error, msg:
-				s = None
-				continue
-			try:
-				s.connect(sa)
-			except socket.error, msg:
-				s.close()
-				s = None
-				continue
-			break
-		if s is None:
-			print('could not open socket')
-			sys.exit(1)
-		s.send('SOCKETOPEN' + "{0} started!".format(self.getName()))
-		data = s.recv(1024)
-		s.close()
-		print('Recieved: ', repr(data))
+                time.sleep(5)
+                for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+                        af, socktype, proto, canonname, sa = res
+                        try:
+                                s = socket.socket(af, socktype, proto)
+                        except socket.error, msg:
+                                s = None
+                                continue
+                        try:
+                                s.connect(sa)
+                        except socket.error, msg:
+                                s.close()
+                                s = None
+                                continue
+                        break
+                if s is None:
+                        print('could not open socket')
+                        sys.exit(1)
+                #s.send('SOCKET OPEN: ' + "{0} started!".format(self.getName()))
+                time.sleep(5)
+                m = "Client-" +str(random.randint(1,proc_number)) + ":"  +"{0}".format(self.getName()) +': ' + str(random.randint(1,10)) + ' widgets requested'
+                s.send(m)
+                #print("{0} asking".format(self.getName()))
+                while 1:
+                        data = s.recv(1000)
+                        if not data: break
+                        print(data + ' is the buyer to me, {0}'.format(self.getName()))
+                        break
+
+                s.close()
+                #print('Recieved: ', repr(data))
 
         #I AM THE CLIENT PROCESS, I CREATE STUFF
 
 
- 
-if __name__ == '__main__':	
-	for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-		af, socktype, proto, canonname, sa = res
-		try:
-			s = socket.socket(af, socktype, proto)
-		except socket.error, msg:
-			s = None
-			continue
-		try:
-			s.bind(sa)
-			for x in range(int(proc_number)):                                     # Four times...
-				mythread = MyThread(name = "Client-{0}".format(x + 1))  # ...Instantiate a thread and pass a unique ID to it
-				mythread.start() 
-		#threads all started, with client sockets being created        
-			s.listen(proc_number)
-		except socket.error, msg:
-			s.close()
-			s = None
-			continue
-		break
-		if s is None:
-			print('could not open socket')
-			sys.exit(1)                                 
+
+if __name__ == '__main__':
+        for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+                af, socktype, proto, canonname, sa = res
+                try:
+                        s = socket.socket(af, socktype, proto)
+                except socket.error, msg:
+                        print('could not open socket pt1')
+                        s = None
+                        continue
+                try:
+                        s.bind(sa)
+                        for x in range(int(proc_number)):                                     # Four times...
+                                mythread = MyThread(name = "Client-{0}".format(x + 1))
+                                mythread.start()
+                #threads all started, with client sockets being created
+                        s.listen(proc_number)
+                except socket.error, msg:
+                        s.close()
+                        s = None
+                        continue
+                break
+        if s is None:
+                print('could not open socket')
+                sys.exit(1)
 
 def starter( ):
-	while 1:
-		conn, addr = s.accept()
-		print ('Connected by', addr)
-		reapChildren( )
-		childPid = os.fork( )
-		if childPid ==0:
-			handleClient(conn)
-		else:
-			activeChildren.append(childPid)
+        while 1:
+                conn, addr = s.accept()
+                #print ('Connected by', addr)
+                reapChildren( )
+                childPid = os.fork( )
+                activeSockets.append(conn)
+                if childPid ==0:
+                        handleClient(conn)
+                else:
+                        activeChildren.append(childPid)
 
 activeChildren = []
+activeSockets = []
+ClientSocket = []
 def reapChildren( ):                              # reap any dead child processes
     while activeChildren:                          # else may fill up system table
         pid,stat = os.waitpid(0, os.WNOHANG)       # don't hang if no child exited
@@ -85,12 +99,36 @@ def reapChildren( ):                              # reap any dead child processe
         activeChildren.remove(pid)
 
 def handleClient(conn):
-	while 1:
-		data = conn.recv(1000)
-		if not data: break
-		print (data) # this will eventually have the snapshot in it to print
-		break
-	conn.close()
-	os._exit(0)
+        while activeSockets.amount != proc_number:
+                print('waiting')
+                time.sleep(5)
+        while 1:
+                data = conn.recv(1000)
+                if not data: break
+                print (data) # this will eventually have the snapshot in it to print
+                buy = data.split(':')[1]
+                sell = data.split(':')[0]
+                ClientSocket.append(buy)
 
-starter( )	   
+                print('sell ' + sell)
+                print('buy ' + buy)
+                time.sleep(10)
+                print (ClientSocket)
+        #       print (activeSockets)
+                time.sleep(10)
+                i = ClientSocket.index(sell)
+                print(sell + " : " + str(i) + ":" + str(activeSockets[i]))
+                #conn.send('recieved data from ' + buy + ' for ' + sell)
+
+                print(sell + " : " + str(i) + ":" + str(activeSockets[i]))
+                #conn.send('recieved data from ' + buy + ' for ' + sell)
+                (activeSockets[i]).send('recieved data from ' + buy)
+                #print (ClientSocket)
+                break
+
+        conn.close()
+        print('disconnected client ')
+        os._exit(0)
+
+
+starter( )
